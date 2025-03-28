f7694f5b2068202d514de89ee6e79ab1bcf03521
SKKUGoon <baesangil0906@gmail.com>
2025-03-28 09:23:42 +0900

    add: multiple streams

--- streams/subscribe.go
+++ streams/subscribe.go
@@ -10,7 +10,7 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-func SubscribeKline(symbol, interval string, handleKline func(internal.KlineData)) error {
+func SubscribeKline(symbol, interval string, ch chan internal.KlineData, done chan struct{}) error {
 	// Binance Futures WebSocket endpoint
 	url := fmt.Sprintf("wss://fstream.binance.com/stream?streams=%s@kline_%s",
 		strings.ToLower(symbol), interval)
@@ -24,19 +24,71 @@ func SubscribeKline(symbol, interval string, handleKline func(internal.KlineData
 
 	log.Printf("Connected to Binance Futures stream for %s", symbol)
 
+	var streamData internal.Stream[internal.KlineData]
 	for {
-		_, message, err := conn.ReadMessage()
-		if err != nil {
-			return fmt.Errorf("error reading message: %v", err)
-		}
+		select {
+		case <-done:
+			close(ch)
+			return nil
+		default:
+			_, message, err := conn.ReadMessage()
+			if err != nil {
+				return fmt.Errorf("error reading message: %v", err)
+			}
 
-		var streamData internal.Stream[internal.KlineData]
-		if err := json.Unmarshal(message, &streamData); err != nil {
-			log.Printf("Error unmarshaling message: %v", err)
-			continue
-		}
+			if err := json.Unmarshal(message, &streamData); err != nil {
+				log.Printf("Error unmarshaling message: %v", err)
+				continue
+			}
 
-		// Process the kline data using the provided handler
-		handleKline(streamData.Data)
+			// Process the kline data using the provided handler
+			ch <- streamData.Data
+		}
+	}
+}
+
+func SubscribeKlineMulti(symbols []string, interval string, chMap map[string]chan internal.KlineData, done chan struct{}) error {
+	urlBase := "wss://fstream.binance.com/stream?streams="
+	streams := make([]string, len(symbols))
+	for i := range symbols {
+		symbol := symbols[i]
+		streams[i] = fmt.Sprintf("%s@kline_%s", strings.ToLower(symbol), interval)
+	}
+
+	url := urlBase + strings.Join(streams, "/")
+	conn, _, err := websocket.DefaultDialer.Dial(url, nil)
+	if err != nil {
+		return fmt.Errorf("websocket connection failed: %v", err)
+	}
+	defer conn.Close()
+
+	log.Printf("Connected to Binance Futures stream for %s", strings.Join(symbols, ", "))
+
+	var streamData internal.Stream[internal.KlineData]
+	for {
+		select {
+		case <-done:
+			for _, symbol := range symbols {
+				close(chMap[symbol])
+			}
+			err := conn.WriteMessage(websocket.CloseMessage, []byte{})
+			if err != nil {
+				log.Printf("Error closing connection: %v", err)
+			}
+			println("Connection to Binance Futures stream for %s is closed", strings.Join(symbols, ", "))
+			return nil
+		default:
+			_, message, err := conn.ReadMessage()
+			if err != nil {
+				return fmt.Errorf("error reading message: %v", err)
+			}
+
+			if err := json.Unmarshal(message, &streamData); err != nil {
+				log.Printf("Error unmarshaling message: %v", err)
+				continue
+			}
+
+			chMap[streamData.Data.Symbol] <- streamData.Data
+		}
 	}
 }
--- streams/subscribe_test.go
+++ streams/subscribe_test.go
@@ -2,6 +2,7 @@ package streams_test
 
 import (
 	"testing"
+	"time"
 
 	"cryptoquant.com/m/internal"
 	"cryptoquant.com/m/streams"
@@ -9,7 +10,54 @@ import (
 
 func TestSubscribeKline(t *testing.T) {
 	t.Log("Starting Stream BTCUSDT 1m")
-	streams.SubscribeKline("BTCUSDT", "1m", func(kline internal.KlineData) {
+	ch := make(chan internal.KlineData)
+	done := make(chan struct{})
+	go streams.SubscribeKline("BTCUSDT", "1m", ch, done)
+
+	// Send done signal after 10 seconds
+	go func() {
+		time.Sleep(10 * time.Second)
+		done <- struct{}{}
+	}()
+
+	for kline := range ch {
 		t.Log(kline)
-	})
+	}
+}
+
+func TestSubscribeKlineMulti(t *testing.T) {
+	t.Log("Starting Stream BTCUSDT 1m")
+	chMap := make(map[string]chan internal.KlineData)
+	chMap["BTCUSDT"] = make(chan internal.KlineData)
+	chMap["ETHUSDT"] = make(chan internal.KlineData)
+	chMap["XRPUSDT"] = make(chan internal.KlineData)
+	chMap["DOGEUSDT"] = make(chan internal.KlineData)
+	chMap["LINKUSDT"] = make(chan internal.KlineData)
+
+	symbols := []string{"BTCUSDT", "ETHUSDT", "XRPUSDT", "DOGEUSDT", "LINKUSDT"}
+	done := make(chan struct{})
+	go streams.SubscribeKlineMulti(symbols, "1m", chMap, done)
+
+	// Send done signal after 10 seconds
+	go func() {
+		time.Sleep(10 * time.Second)
+		done <- struct{}{}
+	}()
+
+	for {
+		select {
+		case <-done:
+			return
+		case kline := <-chMap["BTCUSDT"]:
+			t.Log(kline)
+		case kline := <-chMap["ETHUSDT"]:
+			t.Log(kline)
+		case kline := <-chMap["XRPUSDT"]:
+			t.Log(kline)
+		case kline := <-chMap["DOGEUSDT"]:
+			t.Log(kline)
+		case kline := <-chMap["LINKUSDT"]:
+			t.Log(kline)
+		}
+	}
 }
